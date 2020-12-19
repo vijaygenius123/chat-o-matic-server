0705f5dbf7cfa76176e60272667ed849394bdcd0
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-19 22:31:06 +0530

    Added Subscription

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga')
+const {GraphQLServer, PubSub} = require('graphql-yoga')
 
 const messages = []
 const typeDefs = `
@@ -15,25 +15,45 @@ const typeDefs = `
     type Mutation{
         postMessage(user: String!, content: String!): ID!
     }
+    
+    type Subscription{
+        messages: [Message!]
+     }
+    
 `
+
+const subscribers = []
+const onMessageUpdates = (fn) => subscribers.push(fn)
 
 const resolvers = {
     Query: {
         messages: () => messages
     },
-    Mutation:{
+    Mutation: {
         postMessage: (parent, {user, content}) => {
             const id = messages.length;
             messages.push({
                 id, user, content
             })
+            subscribers.forEach((fn) => fn())
             return id;
+        }
+    },
+    Subscription: {
+        messages: {
+            subscribe: (parent, args, { pubsub }) => {
+                const channel = Math.random().toString(36).slice(2,15);
+                onMessageUpdates(() => pubsub.publish(channel, {messages}));
+                setTimeout(() => pubsub.publish(channel, { messages }),0);
+                return pubsub.asyncIterator(channel)
+            }
         }
     }
 }
 
-const server = new GraphQLServer({typeDefs, resolvers})
+const pubsub = new PubSub()
+const server = new GraphQLServer({typeDefs, resolvers, context: { pubsub }})
 
-server.start(({port})=> {
+server.start(({port}) => {
     console.log(`Server on http://localhost:${port}`)
 });
